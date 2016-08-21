95f0a6a4699e7ca9b7eb8c87a8217c973cabe200
vauxhall <zero.griffin@gmail.com>
2016-08-21 17:15:21 -0500

    singleton user

--- gandalla/Model/User/MUser.swift
+++ gandalla/Model/User/MUser.swift
@@ -2,5 +2,16 @@ import Foundation
 
 class MUser
 {
+    static let sharedInstance = MUser()
     
+    private init()
+    {
+    }
+    
+    //MARK: public
+    
+    func load()
+    {
+        
+    }
 }
