349f4f0c47492b05e9aa179fa208a5a2b7ea5be0
vauxhall <zero.griffin@gmail.com>
2016-08-21 18:12:26 -0500

    firebase database

--- gandalla/Controller/News/CNews.swift
+++ gandalla/Controller/News/CNews.swift
@@ -4,6 +4,13 @@ class CNews:CMainController
 {
     weak var viewNews:VNews!
     
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+        
+        MUser.sharedInstance.load()
+    }
+    
     override func loadView()
     {
         let viewNews:VNews = VNews(controller:self)
--- gandalla/Firebase/Database/Base/FDatabase.swift
+++ gandalla/Firebase/Database/Base/FDatabase.swift
@@ -5,11 +5,17 @@ class FDatabase
 {
     private let reference:FIRDatabaseReference
     private let kReferenceUser:String = "user"
-    private let kReferencePoem:String = "gandaller"
     
     init()
     {
         reference = FIRDatabase.database().reference()
+        
+        let userReference:FIRDatabaseReference = reference.child("pedro")
+        userReference.observeSingleEventOfType(FIRDataEventType.Value, withBlock:
+        { (snap) in
+            
+            print("value:::: \(snap.value)")
+        })
     }
     
     //MARK: public -
@@ -17,7 +23,7 @@ class FDatabase
     
     func createUser() -> String
     {
-        let newUser:FIRDatabaseReference = reference.child(kReferenceUser).childByAutoId()
+        let newUser:FIRDatabaseReference = reference.child("pedro").childByAutoId()
         let newUserId:String = newUser.key
         
         return newUserId
