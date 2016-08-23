487c15112a1d2b3ca6226e6f650fcc44ba3b2728
vauxhall <zero.griffin@gmail.com>
2016-08-23 12:53:37 -0500

    category notification

--- gandalla/Category/CategoryNSNotification.swift
+++ gandalla/Category/CategoryNSNotification.swift
@@ -2,20 +2,8 @@ import Foundation
 
 extension NSNotification
 {
-    class func observeUserSynced(receiver:AnyObject, sel:Selector)
+    enum NSNotificationName:String
     {
-        NSNotificationCenter.defaultCenter().addObserver(receiver, selector:sel, name:userSyncedName(), object:nil)
-    }
-    
-    class func postUserSynced()
-    {
-        NSNotificationCenter.defaultCenter().postNotificationName(userSyncedName(), object:nil)
-    }
-    
-    //MARK: private
-    
-    private class func userSyncedName() -> String
-    {
-        return "userSynced"
+        case GandallersLoaded = "gandallersLoaded"
     }
 }
