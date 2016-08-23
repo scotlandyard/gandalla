3ff8a673b830507f19e346b71c58159a6ff702c5
vauxhall <zero.griffin@gmail.com>
2016-08-23 10:00:07 -0500

    changed activated to started

---
+++ gandalla/View/Create/VCreateDetailCellStarted.swift
@@ -0,0 +1,55 @@
+import UIKit
+
+class VCreateDetailCellStarted:VCreateDetailCell
+{
+    weak var check:UISwitch!
+    
+    override init(frame:CGRect)
+    {
+        super.init(frame:frame)
+        
+        let check:UISwitch = UISwitch()
+        check.translatesAutoresizingMaskIntoConstraints = false
+        check.onTintColor = UIColor.complement()
+        self.check = check
+        
+        let label:UILabel = UILabel()
+        label.userInteractionEnabled = false
+        label.backgroundColor = UIColor.clearColor()
+        label.userInteractionEnabled = false
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.font = UIFont.bold(16)
+        label.textColor = UIColor(white:0.4, alpha:1)
+        label.text = NSLocalizedString("", comment:"")
+        
+        addSubview(label)
+        addSubview(check)
+        
+        let views:[String:AnyObject] = [
+            "check":check,
+            "label":label]
+        
+        let metrics:[String:AnyObject] = [:]
+        
+        addConstraints(NSLayoutConstraint.constraintsWithVisualFormat(
+            "H:[label(150)]-0-[check]-20-|",
+            options:[],
+            metrics:metrics,
+            views:views))
+        addConstraints(NSLayoutConstraint.constraintsWithVisualFormat(
+            "V:|-20-[check]",
+            options:[],
+            metrics:metrics,
+            views:views))
+        addConstraints(NSLayoutConstraint.constraintsWithVisualFormat(
+            "V:|-20-[label(26)]",
+            options:[],
+            metrics:metrics,
+            views:views))
+    }
+    
+    required init?(coder:NSCoder)
+    {
+        fatalError()
+    }
+}
