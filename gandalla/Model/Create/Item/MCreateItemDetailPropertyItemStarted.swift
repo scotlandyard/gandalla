de4c836e7202a8d7541052ca1a8abd2fc4b7e5be
vauxhall <zero.griffin@gmail.com>
2016-08-23 10:42:43 -0500

    creating news

--- gandalla/Model/Create/Item/MCreateItemDetailPropertyItemStarted.swift
+++ gandalla/Model/Create/Item/MCreateItemDetailPropertyItemStarted.swift
@@ -33,11 +33,21 @@ class MCreateItemDetailPropertyItemStarted:MCreateItemDetailPropertyItem
         if check.on
         {
             check.userInteractionEnabled = false
-            let gandallerId:String = controller.model.gandallerId
-            let property:String = FDatabaseModelGandaller.FDatabaseModelGandallerKey.Started.rawValue
-            started = true
             
-            FMain.sharedInstance.database.updateGandaller(gandallerId, property:property, value:started)
+            dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_BACKGROUND, 0))
+            { [weak self] in
+                
+                if self != nil
+                {
+                    self!.started = true
+                    let gandallerId:String = self!.controller.model.gandallerId
+                    let property:String = FDatabaseModelGandaller.FDatabaseModelGandallerKey.Started.rawValue
+                    let fNews:FDatabaseModelNewsJoined = FDatabaseModelNewsJoined(gandallerId:gandallerId)
+                    
+                    FMain.sharedInstance.database.updateGandaller(gandallerId, property:property, value:self!.started)
+                    FMain.sharedInstance.database.createNews(fNews)
+                }
+            }
         }
     }
 }
