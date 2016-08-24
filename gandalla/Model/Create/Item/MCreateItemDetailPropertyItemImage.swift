e8ff0f4bc75c8a3840b50645ac237555d131a915
vauxhall <zero.griffin@gmail.com>
2016-08-24 10:03:05 -0500

    item image

---
+++ gandalla/Model/Create/Item/MCreateItemDetailPropertyItemImage.swift
@@ -0,0 +1,16 @@
+import UIKit
+
+class MCreateItemDetailPropertyItemImage:MCreateItemDetailPropertyItem
+{
+    var image:UIImage?
+    
+    init(image:UIImage?)
+    {
+        self.image = image
+    }
+    
+    override func config(controller:CCreateDetail, cell:VCreateDetailCell)
+    {
+        super.config(controller, cell:cell)
+    }
+}
