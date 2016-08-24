a53d4b2b6baac9212dd7cac323228175650eeee0
vauxhall <zero.griffin@gmail.com>
2016-08-23 19:56:09 -0500

    news and gandaller

--- gandalla/Model/News/MNewsItem.swift
+++ gandalla/Model/News/MNewsItem.swift
@@ -4,12 +4,14 @@ class MNewsItem
 {
     let newsId:String
     let fModel:FDatabaseModelNews
-//    weak var fModel:FDatabaseModelGandaller!
+    weak var gandaller:MGandallerItem!
     
     init(newsId:String, fModel:FDatabaseModelNews)
     {
         self.newsId = newsId
         self.fModel = fModel
         
+        let gandallerId:String = fModel.gandallerId
+        gandaller = MGandaller.sharedInstance.items[gandallerId]
     }
 }
