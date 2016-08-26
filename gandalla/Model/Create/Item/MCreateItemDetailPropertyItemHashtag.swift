20247990a73c6510b8bda92f9bca263d592ec851
vauxhall <zero.griffin@gmail.com>
2016-08-26 12:31:13 -0500

    hashtag item

--- gandalla/Model/Create/Item/MCreateItemDetailPropertyItemHashtag.swift
+++ gandalla/Model/Create/Item/MCreateItemDetailPropertyItemHashtag.swift
@@ -1,9 +1,61 @@
-//
-//  MCreateItemDetailPropertyItemHashtag.swift
-//  gandalla
-//
-//  Created by zero on 8/26/16.
-//  Copyright © 2016 iturbide. All rights reserved.
-//
+import UIKit
 
-import Foundation
+class MCreateItemDetailPropertyItemHashtag:MCreateItemDetailPropertyItem, UITextFieldDelegate
+{
+    let gandallerId:String
+    var fHashtag:FDatabaseModelGandallerSocialHashtag?
+    
+    init(gandallerId:String, fHashtag:FDatabaseModelGandallerSocialHashtag)
+    {
+        self.gandallerId = gandallerId
+        self.fHashtag = fHashtag
+        
+        super.init()
+    }
+    
+    override func config(controller:CCreateDetail, cell:VCreateDetailCell)
+    {
+        super.config(controller, cell:cell)
+        
+        let cellText:VCreateDetailCellText = cell as! VCreateDetailCellText
+        cellText.field.delegate = self
+        cellText.field.text = fHashtag?.tag
+    }
+    
+    //MARK: field del
+    
+    func textFieldDidEndEditing(textField:UITextField)
+    {
+        let newTag:String = textField.text!
+        
+        dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_BACKGROUND, 0))
+        { [weak self] in
+            
+            if self != nil
+            {
+                if self!.fHashtag != nil
+                {
+                    let hashtagId:String = self!.fHashtag!.hashtagId!
+                    let reference:FDatabase.FDatabaseReference = FDatabase.FDatabaseReference.Gandaller
+                    let propertySocial:String = FDatabaseModelGandaller.FDatabaseModelGandallerKey.Social.rawValue
+                    let propertySocialHashtags:String = FDatabaseModelGandaller.FDatabaseModelGandallerKey.SocialHashtags.rawValue
+                    
+                    FMain.sharedInstance.database.updateSubProperty(
+                        reference,
+                        childId:self!.gandallerId,
+                        property:propertySocial,
+                        subChildId:propertySocialHashtags,
+                        subPropertyId:hashtagId,
+                        value:newTag)
+                }
+            }
+        }
+    }
+    
+    func textFieldShouldReturn(textField:UITextField) -> Bool
+    {
+        textField.resignFirstResponder()
+        
+        return true
+    }
+}
