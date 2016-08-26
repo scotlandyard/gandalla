b43febc5e6691a8c24f836657b61509b335525a9
vauxhall <zero.griffin@gmail.com>
2016-08-26 17:02:30 -0500

    gandallers cell and flow

---
+++ gandalla/View/Gandallers/VGandallersCell.swift
@@ -0,0 +1,16 @@
+import UIKit
+
+class VGandallersCell:UICollectionViewCell
+{
+    override init(frame:CGRect)
+    {
+        super.init(frame:frame)
+        clipsToBounds = true
+        backgroundColor = UIColor.clearColor()
+    }
+    
+    required init?(coder:NSCoder)
+    {
+        fatalError()
+    }
+}
