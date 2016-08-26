ccd9e8f18de46be821e52f37268d6aeba686e406
vauxhall <zero.griffin@gmail.com>
2016-08-26 12:22:14 -0500

    twitter social

--- gandalla/Model/Create/Item/MCreateItemDetailPropertyItemTwitter.swift
+++ gandalla/Model/Create/Item/MCreateItemDetailPropertyItemTwitter.swift
@@ -1,9 +1,55 @@
-//
-//  MCreateItemDetailPropertyItemTwitter.swift
-//  gandalla
-//
-//  Created by zero on 8/26/16.
-//  Copyright © 2016 iturbide. All rights reserved.
-//
+import UIKit
 
-import Foundation
+class MCreateItemDetailPropertyItemTwitter:MCreateItemDetailPropertyItem, UITextFieldDelegate
+{
+    var name:String
+    
+    init(name:String)
+    {
+        self.name = name
+        super.init()
+    }
+    
+    override func config(controller:CCreateDetail, cell:VCreateDetailCell)
+    {
+        super.config(controller, cell:cell)
+        
+        let cellText:VCreateDetailCellText = cell as! VCreateDetailCellText
+        cellText.field.delegate = self
+        cellText.field.text = name
+    }
+    
+    //MARK: field del
+    
+    func textFieldDidEndEditing(textField:UITextField)
+    {
+        dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_BACKGROUND, 0))
+        { [weak self] in
+            
+            let name:String = textField.text!
+            
+            if self != nil
+            {
+                self!.name = name
+                let reference:FDatabase.FDatabaseReference = FDatabase.FDatabaseReference.Gandaller
+                let gandallerId:String = self!.controller.model.gandaller.gandallerId
+                let subProperty:String = FDatabaseModelGandaller.FDatabaseModelGandallerKey.Social.rawValue
+                let subChild:String = FDatabaseModelGandaller.FDatabaseModelGandallerKey.SocialTwitter.rawValue
+                let property:String = "\(subProperty)/\(subChild)"
+                
+                FMain.sharedInstance.database.updateProperty(
+                    reference,
+                    childId:gandallerId,
+                    property:property,
+                    value:name)
+            }
+        }
+    }
+    
+    func textFieldShouldReturn(textField:UITextField) -> Bool
+    {
+        textField.resignFirstResponder()
+        
+        return true
+    }
+}
