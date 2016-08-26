029a2721ff68c29aa67a0a495322bd564193c734
vauxhall <zero.griffin@gmail.com>
2016-08-26 16:59:15 -0500

    model in gandallers list

--- gandalla/Controller/Gandallers/CGandallers.swift
+++ gandalla/Controller/Gandallers/CGandallers.swift
@@ -3,10 +3,34 @@ import UIKit
 class CGandallers:CMainController
 {
     weak var viewGandallers:VGandallers!
+    let model:MGandallerList
+    
+    init()
+    {
+        model = MGandallerList()
+        super.init(nibName:nil, bundle:nil)
+    }
+    
+    required init?(coder:NSCoder)
+    {
+        fatalError()
+    }
+    
+    deinit
+    {
+        NSNotificationCenter.defaultCenter().removeObserver(self)
+    }
     
     override func viewDidLoad()
     {
         super.viewDidLoad()
+        
+        generateModel()
+        NSNotificationCenter.defaultCenter().addObserver(
+            self,
+            selector:#selector(self.notifiedGandallersUpdated(sender:)),
+            name:NSNotification.NSNotificationName.GandallersLoaded.rawValue,
+            object:nil)
     }
     
     override func loadView()
@@ -14,5 +38,28 @@ class CGandallers:CMainController
         let viewGandallers:VGandallers = VGandallers(controller:self)
         self.viewGandallers = viewGandallers
         view = viewGandallers
+    }
+    
+    //MARK: notified
+    
+    func notifiedGandallersUpdated(sender notification:NSNotification)
+    {
+        
+    }
+    
+    //MARK: private
+    
+    private func generateModel()
+    {
+        dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_BACKGROUND, 0))
+        { [weak self] in
+            
+            self?.model.load()
+            
+            dispatch_async(dispatch_get_main_queue())
+            { [weak self] in
+                
+            }
+        }
     }
 }
---
+++ gandalla/Model/GandallerList/MGandallerList.swift
@@ -0,0 +1,17 @@
+import Foundation
+
+class MGandallerList
+{
+    var items:[MGandallerListItem]
+    
+    init()
+    {
+        items = []
+    }
+    
+    //MARK: public
+    
+    func load()
+    {
+    }
+}
