d7fa1044747fb30f35292b5210653a74e0545a3f
vauxhall <zero.griffin@gmail.com>
2016-08-26 10:09:19 -0500

    model items

---
+++ gandalla/Model/Create/Item/MCreateItemDetailPropertyItemHashtag.swift
@@ -0,0 +1,9 @@
+//
+//  MCreateItemDetailPropertyItemHashtag.swift
+//  gandalla
+//
+//  Created by zero on 8/26/16.
+//  Copyright © 2016 iturbide. All rights reserved.
+//
+
+import Foundation
---
+++ gandalla/Model/Create/Section/MCreateItemDetailPropertyVideos.swift
@@ -0,0 +1,9 @@
+//
+//  MCreateItemDetailPropertyVideos.swift
+//  gandalla
+//
+//  Created by zero on 8/26/16.
+//  Copyright © 2016 iturbide. All rights reserved.
+//
+
+import Foundation
