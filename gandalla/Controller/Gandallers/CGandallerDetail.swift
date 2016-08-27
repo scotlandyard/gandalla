7538d68cd4cec16e6ae5412d68da79fc83dfbdec
vauxhall <zero.griffin@gmail.com>
2016-08-27 15:33:17 -0500

    controller gandaller detail

--- gandalla/Controller/Gandallers/CGandallerDetail.swift
+++ gandalla/Controller/Gandallers/CGandallerDetail.swift
@@ -2,5 +2,24 @@ import UIKit
 
 class CGandallerDetail:CMainController
 {
+    weak var viewDetail:VGandallerDetail!
+    weak var model:MGandallerItem!
     
+    init(model:MGandallerItem)
+    {
+        self.model = model
+        super.init(nibName:nil, bundle:nil)
+    }
+    
+    required init?(coder:NSCoder)
+    {
+        fatalError()
+    }
+    
+    override func loadView()
+    {
+        let viewDetail:VGandallerDetail = VGandallerDetail(controller:self)
+        self.viewDetail = viewDetail
+        view = viewDetail
+    }
 }
