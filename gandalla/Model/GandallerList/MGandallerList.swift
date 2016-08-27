c4192f86b4a5f5f3e0620fea7367ca1d8a294400
vauxhall <zero.griffin@gmail.com>
2016-08-27 01:49:50 -0500

    gandaller parsing

--- gandalla/Model/GandallerList/MGandallerList.swift
+++ gandalla/Model/GandallerList/MGandallerList.swift
@@ -13,13 +13,20 @@ class MGandallerList
     
     func load()
     {
+        var newItems:[MGandallerListItem] = []
         let gandallersKeys:[String] = Array(MGandaller.sharedInstance.items.keys)
         
         for gandallerKey:String in gandallersKeys
         {
-            let gandaller:MGandallerItem = MGandaller.sharedInstance.items[gandallerKey]
+            let gandaller:MGandallerItem = MGandaller.sharedInstance.items[gandallerKey]!
             
             if gandaller.fModel.started
+            {
+                if gandaller.fModel.status == FDatabaseModelGandaller.FDatabaseModelGandallerStatus.Active
+                {
+                    
+                }
+            }
         }
     }
 }
