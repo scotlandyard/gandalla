b6b6ccb0f00bbe2eaadba5edcdf162cde85c3ce4
vauxhall <zero.griffin@gmail.com>
2016-08-28 00:28:21 -0500

    cell video

--- gandalla/View/GandallerDetail/VGandallerDetailCellVideo.swift
+++ gandalla/View/GandallerDetail/VGandallerDetailCellVideo.swift
@@ -2,13 +2,50 @@ import UIKit
 
 class VGandallerDetailCellVideo:VGandallerDetailCell
 {
+    weak var label:UILabel!
+    
     override init(frame:CGRect)
     {
         super.init(frame:frame)
+        backgroundColor = UIColor.whiteColor()
+        userInteractionEnabled = false
+        
+        let label:UILabel = UILabel()
+        label.userInteractionEnabled = false
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.backgroundColor = UIColor.clearColor()
+        label.font = UIFont.regular(12)
+        label.textColor = UIColor(white:0.3, alpha:1)
+        self.label = label
+        
+        addSubview(label)
+        
+        let views:[String:AnyObject] = [
+            "label":label]
+        
+        let metrics:[String:AnyObject] = [:]
+        
+        addConstraints(NSLayoutConstraint.constraintsWithVisualFormat(
+            "H:|-10-[label]-10-|",
+            options:[],
+            metrics:metrics,
+            views:views))
+        addConstraints(NSLayoutConstraint.constraintsWithVisualFormat(
+            "V:|-0-[label]-0-|",
+            options:[],
+            metrics:metrics,
+            views:views))
     }
     
     required init?(coder:NSCoder)
     {
         super.init(coder:coder)
+    }
+    
+    override func config(model:MGandallerDetailItem)
+    {
+        let modelVideo:MGandallerDetailItemVideo = model as! MGandallerDetailItemVideo
+        let url:String = modelVideo.url
+        label.text = url
     }
 }
