f5980903538120fd028da956a84fc2fe17c5e953
vauxhall <zero.griffin@gmail.com>
2016-08-28 18:46:51 -0500

    controller uses view champions

--- gandalla/Controller/Champions/CChampions.swift
+++ gandalla/Controller/Champions/CChampions.swift
@@ -2,5 +2,13 @@ import UIKit
 
 class CChampions:CMainController
 {
-    weak var viewChampions
+    var model:MChampions?
+    weak var viewChampions:VChampions!
+    
+    override func loadView()
+    {
+        let viewChampions:VChampions = VChampions(controller:self)
+        self.viewChampions = viewChampions
+        view = viewChampions
+    }
 }
