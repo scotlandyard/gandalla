afc20631f845780a2214b5d61b8a47142ed496ef
vauxhall <zero.griffin@gmail.com>
2016-08-27 20:20:17 -0500

    view cell power

---
+++ gandalla/View/GandallerDetail/VGandallerDetailCellPower.swift
@@ -0,0 +1,14 @@
+import UIKit
+
+class VGandallerDetailCellPower:VGandallerDetailCell
+{
+    override init(frame:CGRect)
+    {
+        super.init(frame:frame)
+    }
+    
+    required init?(coder:NSCoder)
+    {
+        super.init(coder:coder)
+    }
+}
