685db1e57baee9b055c8760b2c6f24014856281d
vauxhall <zero.griffin@gmail.com>
2016-08-27 20:04:41 -0500

    model social

--- gandalla/Model/GandallerDetail/MGandallerDetailItemSocial.swift
+++ gandalla/Model/GandallerDetail/MGandallerDetailItemSocial.swift
@@ -1,9 +1,13 @@
-//
-//  MGandallerDetailItemSocial.swift
-//  gandalla
-//
-//  Created by zero on 8/27/16.
-//  Copyright © 2016 iturbide. All rights reserved.
-//
+import UIKit
 
-import Foundation
+class MGandallerDetailItemSocial:MGandallerDetailItem
+{
+    private let kCellHeight:CGFloat = 100
+    
+    init()
+    {
+        let reusableIdentifier:String = VGandallerDetailCellSocial.reusableIdentifier()
+        
+        super.init(reusableIdentifier:reusableIdentifier, cellHeight:kCellHeight)
+    }
+}
