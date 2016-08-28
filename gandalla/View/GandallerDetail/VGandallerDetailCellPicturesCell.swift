7760fc04073a195cf5894cd98318ec4b4058d51a
vauxhall <zero.griffin@gmail.com>
2016-08-28 16:31:27 -0500

    picture cell

--- gandalla/View/GandallerDetail/VGandallerDetailCellPicturesCell.swift
+++ gandalla/View/GandallerDetail/VGandallerDetailCellPicturesCell.swift
@@ -2,16 +2,50 @@ import UIKit
 
 class VGandallerDetailCellPicturesCell:UICollectionViewCell
 {
+    weak var image:UIImageView!
+    
     override init(frame:CGRect)
     {
         super.init(frame:frame)
         clipsToBounds = true
         userInteractionEnabled = false
         backgroundColor = UIColor(white:0.85, alpha:1)
+        
+        let image:UIImageView = UIImageView()
+        image.userInteractionEnabled = false
+        image.clipsToBounds = true
+        image.contentMode = UIViewContentMode.Center
+        image.translatesAutoresizingMaskIntoConstraints = false
+        self.image = image
+        
+        addSubview(image)
+        
+        let views:[String:AnyObject] = [
+            "image":image]
+        
+        let metrics:[String:AnyObject] = [:]
+        
+        addConstraints(NSLayoutConstraint.constraintsWithVisualFormat(
+            "H:|-0-[image]-0-|",
+            options:[],
+            metrics:metrics,
+            views:views))
+        addConstraints(NSLayoutConstraint.constraintsWithVisualFormat(
+            "V:|-0-[image]-0-|",
+            options:[],
+            metrics:metrics,
+            views:views))
     }
     
     required init?(coder:NSCoder)
     {
         fatalError()
+    }
+    
+    //MARK: public
+    
+    func config(model:MGandallerDetailItemPicturesItem)
+    {
+        image.image = model.imageBinary
     }
 }
