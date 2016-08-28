7c901a133cf0583b8a3db4787df50fec3eec5b6c
vauxhall <zero.griffin@gmail.com>
2016-08-28 05:55:22 -0500

    view social

--- gandalla/Model/GandallerDetail/MGandallerDetailItemSocial.swift
+++ gandalla/Model/GandallerDetail/MGandallerDetailItemSocial.swift
@@ -3,12 +3,14 @@ import UIKit
 class MGandallerDetailItemSocial:MGandallerDetailItem
 {
     let title:String
+    let icon:String
     private let kCellHeight:CGFloat = 35
     private let kSelectable:Bool = false
     
     init(title:String, icon:String)
     {
         self.title = title
+        self.icon = icon
         let reusableIdentifier:String = VGandallerDetailCellSocial.reusableIdentifier()
         
         super.init(reusableIdentifier:reusableIdentifier, cellHeight:kCellHeight, selectable:kSelectable)
--- gandalla/View/GandallerDetail/VGandallerDetailCellSocial.swift
+++ gandalla/View/GandallerDetail/VGandallerDetailCellSocial.swift
@@ -2,13 +2,66 @@ import UIKit
 
 class VGandallerDetailCellSocial:VGandallerDetailCell
 {
+    weak var label:UILabel!
+    weak var icon:UIImageView!
+    
     override init(frame:CGRect)
     {
         super.init(frame:frame)
+        backgroundColor = UIColor.whiteColor()
+        userInteractionEnabled = false
+        
+        let label:UILabel = UILabel()
+        label.userInteractionEnabled = false
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.backgroundColor = UIColor.clearColor()
+        label.font = UIFont.regular(15)
+        label.textColor = UIColor(white:0.4, alpha:1)
+        label.numberOfLines = 2
+        self.label = label
+        
+        let icon:UIImageView = UIImageView()
+        icon.userInteractionEnabled = false
+        icon.translatesAutoresizingMaskIntoConstraints = false
+        icon.clipsToBounds = true
+        icon.contentMode = UIViewContentMode.Center
+        self.icon = icon
+        
+        addSubview(label)
+        addSubview(icon)
+        
+        let views:[String:AnyObject] = [
+            "label":label,
+            "icon":icon]
+        
+        let metrics:[String:AnyObject] = [:]
+        
+        addConstraints(NSLayoutConstraint.constraintsWithVisualFormat(
+            "H:|-0-[icon(35)]-0-[label]-10-|",
+            options:[],
+            metrics:metrics,
+            views:views))
+        addConstraints(NSLayoutConstraint.constraintsWithVisualFormat(
+            "V:|-0-[label]-0-|",
+            options:[],
+            metrics:metrics,
+            views:views))
+        addConstraints(NSLayoutConstraint.constraintsWithVisualFormat(
+            "V:|-0-[icon]-0-|",
+            options:[],
+            metrics:metrics,
+            views:views))
     }
     
     required init?(coder:NSCoder)
     {
         super.init(coder:coder)
+    }
+    
+    override func config(model:MGandallerDetailItem)
+    {
+        let modelSocial:MGandallerDetailItemSocial = model as! MGandallerDetailItemSocial
+        label.text = modelSocial.title
+        icon.image = UIImage(named:modelSocial.icon)
     }
 }
