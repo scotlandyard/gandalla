69dc8dfa9fbb1ad05b1b2fe88e62278331f65a76
vauxhall <zero.griffin@gmail.com>
2016-08-28 05:48:30 -0500

    model instagram

---
+++ gandalla/Model/GandallerDetail/MGandallerDetailItemSocialInstagram.swift
@@ -0,0 +1,11 @@
+import UIKit
+
+class MGandallerDetailItemSocialInstagram:MGandallerDetailItemSocial
+{
+    private let kIcon:String = "gandallerSocialInstagram"
+    
+    init(title:String)
+    {
+        super.init(title:title, icon:kIcon)
+    }
+}
