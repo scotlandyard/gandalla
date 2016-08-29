ad525c80527ce02f215033dde079ab6d47d948f8
vauxhall <zero.griffin@gmail.com>
2016-08-28 19:32:30 -0500

    parsing likes

--- gandalla/Model/Champions/MChampions.swift
+++ gandalla/Model/Champions/MChampions.swift
@@ -6,6 +6,19 @@ class MChampions
     
     init(json:[String:AnyObject])
     {
-        items = []
+        var items:[MChampionsItem] = []
+        
+        let countKey:String = FDatabaseModelLike.FDatabaseModelLikeKey.Count.rawValue
+        let jsonKeys:[String] = Array(json.keys)
+        
+        for jsonKey:String in jsonKeys
+        {
+            let jsonItem:[String:AnyObject] = json[jsonKey] as! [String:AnyObject]
+            let count:Int = jsonItem[countKey] as! Int
+            let item:MChampionsItem = MChampionsItem(gandallerId:jsonKey, count:count)
+            items.append(item)
+        }
+        
+        self.items = items
     }
 }
--- gandalla/Model/Champions/MChampionsItem.swift
+++ gandalla/Model/Champions/MChampionsItem.swift
@@ -2,5 +2,12 @@ import Foundation
 
 class MChampionsItem
 {
+    let count:Int
     weak var modelGandaller:MGandallerItem!
+    
+    init(gandallerId:String, count:Int)
+    {
+        self.count = count
+        self.modelGandaller = MGandaller.sharedInstance.items[gandallerId]
+    }
 }
