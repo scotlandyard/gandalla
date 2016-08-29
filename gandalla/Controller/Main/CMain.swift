f18de2b06efa378d389b7d70580c222482ec6aa0
vauxhall <zero.griffin@gmail.com>
2016-08-29 00:14:10 -0500

    loading firebase from main thread

--- gandalla/Controller/Main/CMain.swift
+++ gandalla/Controller/Main/CMain.swift
@@ -8,6 +8,8 @@ class AppDelegate:UIResponder, UIApplicationDelegate
 
     func application(application:UIApplication, didFinishLaunchingWithOptions launchOptions:[NSObject:AnyObject]?) -> Bool
     {
+        FMain.sharedInstance
+        
         let window = UIWindow(frame:UIScreen.mainScreen().bounds)
         window.makeKeyAndVisible()
         window.backgroundColor = UIColor.whiteColor()
