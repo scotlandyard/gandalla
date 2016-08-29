df33fd9b75acf3a755390a96d4a3be9d128383b1
vauxhall <zero.griffin@gmail.com>
2016-08-28 23:09:19 -0500

    sorting liked items

--- gandalla/Model/Liked/MLiked.swift
+++ gandalla/Model/Liked/MLiked.swift
@@ -7,7 +7,39 @@ class MLiked
     init()
     {
         var items:[MLikedItem] = []
+        let dbLikedList:[DGandallaGandaller] = MUser.sharedInstance.dbUser.likedGandaller
         
+        for dbLiked:DGandallaGandaller in dbLikedList
+        {
+            let gandallerId:String = dbLiked.gandallerId
+            let modelGandaller:MGandallerItem? = MGandaller.sharedInstance.items[gandallerId]
+            
+            if modelGandaller != nil
+            {
+                let created:NSTimeInterval = dbLiked.created
+                let item:MLikedItem = MLikedItem(modelGandaller:modelGandaller!, created:created)
+                items.append(item)
+            }
+        }
+        
+        items.sortInPlace()
+        { (itemA, itemB) -> Bool in
+            
+            let before:Bool
+            let createdA:NSTimeInterval = itemA.created
+            let createdB:NSTimeInterval = itemB.created
+            
+            if createdA <= createdB
+            {
+                before = true
+            }
+            else
+            {
+                before = false
+            }
+            
+            return before
+        }
         
         self.items = items
     }
--- gandalla/Model/Liked/MLikedItem.swift
+++ gandalla/Model/Liked/MLikedItem.swift
@@ -2,5 +2,12 @@ import Foundation
 
 class MLikedItem
 {
+    weak var modelGandaller:MGandallerItem!
+    let created:NSTimeInterval
     
+    init(modelGandaller:MGandallerItem, created:NSTimeInterval)
+    {
+        self.modelGandaller = modelGandaller
+        self.created = created
+    }
 }
