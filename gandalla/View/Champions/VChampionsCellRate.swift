c90acfd3a04ca3b6180f70f1f1a7f61a87bf7106
vauxhall <zero.griffin@gmail.com>
2016-08-30 15:55:52 -0500

    cell rate

--- gandalla/View/Champions/VChampionsCellRate.swift
+++ gandalla/View/Champions/VChampionsCellRate.swift
@@ -3,59 +3,23 @@ import UIKit
 class VChampionsCellRate:UIView
 {
     weak var label:UILabel!
+    let colorBackground:UIColor
+    let colorForeground:UIColor
+    private let kLineBaseWidth:CGFloat = 10
+    private let kLineForeWidth:CGFloat = 9
+    private let kMargin:CGFloat = 7
+    private let deg90:CGFloat = 1.5708
     
     init()
     {
+        colorBackground = UIColor(white:0.9, alpha:1)
+        colorForeground = UIColor.complement()
+        
         super.init(frame:CGRectZero)
         userInteractionEnabled = false
         clipsToBounds = true
         translatesAutoresizingMaskIntoConstraints = false
-        
-        let image:UIImageView = UIImageView()
-        image.userInteractionEnabled = false
-        image.translatesAutoresizingMaskIntoConstraints = false
-        image.clipsToBounds = true
-        image.contentMode = UIViewContentMode.Center
-        image.image = UIImage(named:"championsRate")
-        
-        let label:UILabel = UILabel()
-        label.userInteractionEnabled = false
-        label.translatesAutoresizingMaskIntoConstraints = false
-        label.backgroundColor = UIColor.clearColor()
-        label.font = UIFont.numeric(28)
-        label.textColor = UIColor.whiteColor()
-        label.textAlignment = NSTextAlignment.Center
-        self.label = label
-        
-        addSubview(image)
-        addSubview(label)
-        
-        let views:[String:AnyObject] = [
-            "image":image,
-            "label":label]
-        
-        let metrics:[String:AnyObject] = [:]
-        
-        addConstraints(NSLayoutConstraint.constraintsWithVisualFormat(
-            "H:|-0-[image]-0-|",
-            options:[],
-            metrics:metrics,
-            views:views))
-        addConstraints(NSLayoutConstraint.constraintsWithVisualFormat(
-            "V:|-0-[image]-0-|",
-            options:[],
-            metrics:metrics,
-            views:views))
-        addConstraints(NSLayoutConstraint.constraintsWithVisualFormat(
-            "H:|-7-[label]-15-|",
-            options:[],
-            metrics:metrics,
-            views:views))
-        addConstraints(NSLayoutConstraint.constraintsWithVisualFormat(
-            "V:|-0-[label]-0-|",
-            options:[],
-            metrics:metrics,
-            views:views))
+        backgroundColor = UIColor.clearColor()
     }
     
     required init?(coder:NSCoder)
@@ -63,13 +27,41 @@ class VChampionsCellRate:UIView
         fatalError()
     }
     
+    override func drawRect(rect:CGRect)
+    {
+        let width:CGFloat = rect.maxX
+        let height:CGFloat = rect.maxY
+        let width_2:CGFloat = width / 2.0
+        let height_2:CGFloat = height / 2.0
+        let diameter:CGFloat
+        let radius:CGFloat
+        let context:CGContext? = UIGraphicsGetCurrentContext()
+        
+        if width < height
+        {
+            diameter = width
+        }
+        else
+        {
+            diameter = height
+        }
+        
+        radius = (diameter - kMargin) / 2.0
+        
+        CGContextSetStrokeColorWithColor(context, colorBackground.CGColor)
+        CGContextSetLineWidth(context, kLineBaseWidth)
+        CGContextSetLineCap(context, CGLineCap.Round)
+        CGContextMoveToPoint(context, radius, height)
+        CGContextAddArc(context, width_2, height_2, radius, deg90, 0, 1)
+        CGContextDrawPath(context, CGPathDrawingMode.Stroke)
+        
+        CGContextSetStrokeColorWithColor(context, colorBackground.CGColor)
+    }
+    
     //MARK: public
     
-    func count(likes:Int)
+    func count(percentage:CGFloat)
     {
-        let formatter:NSNumberFormatter = NSNumberFormatter()
-        formatter.numberStyle = NSNumberFormatterStyle.DecimalStyle
-        let countString:String = formatter.stringFromNumber(likes)!
-        label.text = countString
+        
     }
 }
