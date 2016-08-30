430715787c85dde05016a2cd89dfd7becbc2b72f
vauxhall <zero.griffin@gmail.com>
2016-08-29 22:09:49 -0500

    instagram

---
+++ gandalla/Model/Create/Item/MCreateItemDetailPropertyItemTextInstagram.swift
@@ -0,0 +1,33 @@
+import UIKit
+
+class MCreateItemDetailPropertyItemTextInstagram:MCreateItemDetailPropertyItemText
+{
+    let gandallerId:String
+    var fSocial:FDatabaseModelGandallerSocial?
+    private let kRemoveAble:Bool = false
+    
+    init(fSocial:FDatabaseModelGandallerSocial, gandallerId:String)
+    {
+        self.fSocial = fSocial
+        self.gandallerId = gandallerId
+        let placeholder:String = NSLocalizedString("MCreateItemDetailPropertyItemInstagram_placeholder", comment:"")
+        super.init(placeholder:placeholder, text:fSocial.insta, removeAble:kRemoveAble)
+    }
+    
+    override func changedText(text:String)
+    {
+        super.changedText(text)
+        
+        fSocial?.twitter = text
+        let reference:FDatabase.FDatabaseReference = FDatabase.FDatabaseReference.Gandaller
+        let subProperty:String = FDatabaseModelGandaller.FDatabaseModelGandallerKey.Social.rawValue
+        let subChild:String = FDatabaseModelGandaller.FDatabaseModelGandallerKey.SocialTwitter.rawValue
+        let property:String = "\(subProperty)/\(subChild)"
+        
+        FMain.sharedInstance.database.updateProperty(
+            reference,
+            childId:gandallerId,
+            property:property,
+            value:text)
+    }
+}
