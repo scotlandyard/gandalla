e4d2f22c80095c81be17e37cc8edf40745f7324e
vauxhall <zero.griffin@gmail.com>
2016-08-30 11:47:37 -0500

    controller notifications

--- gandalla/Controller/Notifications/CNotifications.swift
+++ gandalla/Controller/Notifications/CNotifications.swift
@@ -2,5 +2,31 @@ import UIKit
 
 class CNotifications:CMainController
 {
+    weak var viewNotifications:VNotifications!
+    let model:MNotifications
     
+    init()
+    {
+        model = MNotifications()
+        super.init(nibName:nil, bundle:nil)
+    }
+    
+    required init?(coder:NSCoder)
+    {
+        fatalError()
+    }
+    
+    override func loadView()
+    {
+        let viewNotifications:VNotifications = VNotifications(controller:self)
+        self.viewNotifications = viewNotifications
+        view = viewNotifications
+    }
+    
+    //MARK: public
+    
+    func selectedNotification(index:NSIndexPath)
+    {
+        
+    }
 }
